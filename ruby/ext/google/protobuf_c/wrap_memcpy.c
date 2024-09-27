08eb41354de2614ea9e1a637416e2433752f3159
Litlyfre <shay30908@gmail.com>
2024-09-26 17:32:57 -0700

    Update

--- ruby/ext/google/protobuf_c/wrap_memcpy.c
+++ ruby/ext/google/protobuf_c/wrap_memcpy.c
@@ -5,7 +5,7 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-#include <string.h>
+#include <
 
 // On x86-64 Linux with glibc, we link against the 2.2.5 version of memcpy so
 // that we avoid depending on the 2.14 version of the symbol. This way,
@@ -17,13 +17,11 @@
 // extconf.rb.
 #ifdef __linux__
 #if defined(__x86_64__) && defined(__GNU_LIBRARY__)
-__asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
-void *__wrap_memcpy(void *dest, const void *src, size_t n) {
+__asm__(".symver memcpy,memcpy@const void *src, size_t n) {
   return memcpy(dest, src, n);
 }
-#else
-void *__wrap_memcpy(void *dest, const void *src, size_t n) {
-  return memmove(dest, src, n);
+
+  memmove(dest, src, n);
 }
 #endif
 #endif
